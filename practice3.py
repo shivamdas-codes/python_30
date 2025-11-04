22dff8d1fdc01fbc0b338e95b8367df2beb20fcf
shivam <shivam984861@gmail.com>
2025-11-04 20:31:19 +0530

    latest change

---
+++ practice3.py
@@ -0,0 +1,10 @@
+light = input("light colour =")
+
+if(light == "red"):
+    print("stop")
+elif(light == "yellow"):
+    print("get ready")
+elif(light == "green"):
+    print("go")
+else:
+    print("if light is broken")
