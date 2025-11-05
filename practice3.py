acbb31e9e247d0400729aa2ca100828700b56a94
shivam <shivam984861@gmail.com>
2025-11-05 09:52:25 +0530

    new changes

--- practice3.py
+++ practice3.py
@@ -1,5 +1,6 @@
 # this code indicates about the conditional statements in python, which is mainly used in decision making
-light = input("light colour =")
+# example code 1
+"""light = input("light colour =")
 
 if(light == "red"):
     print("stop")
@@ -8,4 +9,26 @@ elif(light == "yellow"):
 elif(light == "green"):
     print("go")
 else:
-    print("if light is broken")
+    print("if light is broken")"""
+
+
+
+# example code 2
+"""marks = input("marks :")
+if(marks >= "90"):
+    print("grade A")
+elif(marks >= "80" and marks < "90"):
+    print("grade B")
+elif(marks >= "70" and marks < "80"):
+    print("grade C")
+else:
+    print("fail")"""
+
+# in the above code the input statement always return the string not the integer value
+# in the code the "and" operator is used to combine two conditions together
+
+
+
+# example code 3
+
+
