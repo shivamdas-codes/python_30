e9cbcfcde6e904a6edceff30051ffaf628b60e3e
shivam <shivam984861@gmail.com>
2025-11-06 05:53:16 +0530

    new changes

--- practice2.py
+++ practice2.py
@@ -5,3 +5,5 @@ print("my name is",name,"my age is",age)
 """the above code represents the INPUT RULE in the python 
 where word or any sentence can be written in input statement but the numbers or any other decimal value will be assign as their type"""
 
+
+
