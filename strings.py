8b6a01a2af69ac1e1656795350298bf854bb9936
shivam <shivam984861@gmail.com>
2025-11-11 22:24:18 +0530

    new changes

--- strings.py
+++ strings.py
@@ -1,5 +1,13 @@
-# the strings can be written in three ways
-str1 = 'hello world'          # using single quotes
-str2 = "hello world"          # using double quotes
-str3 = '''hello world'''        # using triple quotes
+# # the strings can be written in three ways
+# str1 = 'hello world'          # using single quotes
+# str2 = "hello world"          # using double quotes
+# str3 = '''hello world'''        # using triple quotes
 
+
+str1 = "hey this is shivam das\ncurrently im pursuing my mca"
+# the above line was assigned by '\n' which is used to break the line in the output as two sentences
+str2 = "hey this is shivam das\tcurrently im pursuing my mca"
+# the above line was assigned by '\t' which is used to give a tab space in the output
+
+print(str1 )
+print(str2 )
