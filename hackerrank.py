2a7b8d7c61d6f6bb2466e20a3f77db628c9eb8de
shivam <shivam984861@gmail.com>
2025-11-16 20:26:11 +0530

    new changes

--- hackerrank.py
+++ hackerrank.py
@@ -1,18 +1,30 @@
-"""# (1).Given an integer, n , perform the following conditional actions:
-If n is odd, print Weird
-If n is even and in the inclusive range of  to , print Not Weird
-If n is even and in the inclusive range of  to , print Weird
-If n is even and greater than , print Not Weird"""
+# """# (1).Given an integer, n , perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of  to , print Not Weird
+# If n is even and in the inclusive range of  to , print Weird
+# If n is even and greater than , print Not Weird"""
 
-n = 24
-if(n is "odd" and "odd numbers are weird"):
-    print("weird")
-elif(n is "even" and n in range(2,5)):
-    print("not weird")
-elif(n is "even" and n in range(6,20)):
-    print("weird")
-else:
-    (n > 20 and n is "even")
-    print("not weird")
+# n = 24
+# if(n is "odd" and "odd numbers are weird"):
+#     print("weird")
+# elif(n is "even" and n in range(2,5)):
+#     print("not weird")
+# elif(n is "even" and n in range(6,20)):
+#     print("weird")
+# else:
+#     (n > 20 and n is "even")
+#     print("not weird")
 
     
+
+"""The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers."""
+
+a = int(input())
+b = int(input())
+sum = (a + b)
+diff = (a - b)
+prod = (a * b)
+print(sum,diff,prod)
